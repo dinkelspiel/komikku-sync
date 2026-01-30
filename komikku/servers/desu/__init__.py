5cf76adfcaa4efccbd203c3fa836f07cd91f3935
Valéry Febvre <vfebvre@easter-eggs.com>
2026-01-30 21:26:09 +0100

    [Servers] Desu (RU): Update
    Domain change

--- komikku/servers/desu/__init__.py
+++ komikku/servers/desu/__init__.py
@@ -23,7 +23,7 @@ class Desu(Server):
     lang = 'ru'
     is_nsfw = True
 
-    base_url = 'https://desu.city'
+    base_url = 'https://desu.uno'
     logo_url = base_url + '/styles/favicons/favicon-32x32.png?v=1'
     api_url = base_url + '/manga/api/'
     api_latest_updates_url = base_url + '/manga/api?limit=50&order=updated'

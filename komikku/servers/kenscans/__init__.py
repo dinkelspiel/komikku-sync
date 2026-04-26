7f29826b01aef6736ea6377a728b0f3cc66aa70f
Valéry Febvre <vfebvre@easter-eggs.com>
2026-04-27 01:28:16 +0200

    [Servers] Kenscans (EN): Update
    Fix get_manga_chapter_data() method

--- komikku/servers/kenscans/__init__.py
+++ komikku/servers/kenscans/__init__.py
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Author: Valéry Febvre <vfebvre@easter-eggs.com>
 
+from bs4 import BeautifulSoup
+
 from komikku.servers.multi.heancms import HeanCMS
 
 
@@ -13,3 +15,31 @@ class Kenscans(HeanCMS):
     base_url = 'https://kencomics.com'
     logo_url = base_url + '/favicon-32x32.png'
     api_url = 'https://api.kencomics.com/api'
+
+    def get_manga_chapter_data(self, manga_slug, manga_name, chapter_slug, chapter_url):
+        """
+        Returns manga chapter data by scraping chapter HTML page content
+
+        Pages URLs are available in a <script> element
+        """
+        r = self.session_get(
+            self.chapter_url.format(manga_slug, chapter_slug),
+            headers={
+                'Referer': self.manga_url.format(manga_slug),
+            }
+        )
+        if r.status_code != 200:
+            return None
+
+        soup = BeautifulSoup(r.text, 'lxml')
+
+        data = dict(
+            pages=[],
+        )
+        for image in soup.select('.image-container img'):
+            data['pages'].append(dict(
+                slug=None,
+                image=image.get('src'),
+            ))
+
+        return data

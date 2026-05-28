7f468168e089e2b2d0d2a9e069ad96d032b70be6
Valéry Febvre <vfebvre@easter-eggs.com>
2026-05-28 13:05:35 +0200

    [Card] Info: Replace Synopsis Gtk.Expander by a custom fading widget (SynopsisFading)
    By default, the synopsis is partially visible (first ~4 lines).
    It can be fully displayed using a Gtk.ToggleButton (Show More/Less)

--- komikku/card/__init__.py
+++ komikku/card/__init__.py
@@ -3,25 +3,18 @@
 # Author: Valéry Febvre <vfebvre@easter-eggs.com>
 
 from gettext import gettext as _
-import pytz
 
 from gi.repository import Adw
 from gi.repository import Gdk
 from gi.repository import Gio
 from gi.repository import GLib
 from gi.repository import Gtk
-from gi.repository import Pango
 
-from komikku.consts import COVER_WIDTH
-from komikku.consts import MISSING_IMG_RESOURCE_PATH
-from komikku.consts import TIMEZONE
 from komikku.card.categories_list import CategoriesList
 from komikku.card.chapters_list import ChaptersList
+from komikku.card.info_box import InfoBox
 from komikku.card.tracking import TrackingDialog
-from komikku.models import Category
 from komikku.models import Settings
-from komikku.utils import CoverPicture
-from komikku.utils import folder_size
 from komikku.utils import html_escape
 
 
@@ -64,7 +57,8 @@ class CardPage(Adw.NavigationPage):
     scanlators_label = Gtk.Template.Child('scanlators_label')
     chapters_label = Gtk.Template.Child('chapters_label')
     last_update_label = Gtk.Template.Child('last_update_label')
-    synopsis_label = Gtk.Template.Child('synopsis_label')
+    synopsis_box = Gtk.Template.Child('synopsis_box')
+    synopsis_togglebutton = Gtk.Template.Child('synopsis_togglebutton')
     size_on_disk_label = Gtk.Template.Child('size_on_disk_label')
     viewswitcherbar = Gtk.Template.Child('viewswitcherbar')
 
@@ -541,163 +535,3 @@ class CardPage(Adw.NavigationPage):
     def toggle_resume(self, state):
         self.resume_action.set_enabled(state)
         self.resume_button.set_sensitive(state)
-
-
-class InfoBox:
-    def __init__(self, card):
-        self.card = card
-        self.window = card.window
-
-        self.title_box = self.card.title_box
-        self.cover_box = self.card.cover_box
-        self.cover_picture = None
-        self.name_label = self.card.name_label
-        self.authors_label = self.card.authors_label
-        self.status_server_label = self.card.status_server_label
-        self.buttons_box = self.card.buttons_box
-        self.add_button = self.card.add_button
-        self.resume_button = self.card.resume_button
-        self.genres_wrapbox = self.card.genres_wrapbox
-        self.categories_wrapbox = self.card.categories_wrapbox
-        self.scanlators_label = self.card.scanlators_label
-        self.chapters_label = self.card.chapters_label
-        self.last_update_label = self.card.last_update_label
-        self.synopsis_label = self.card.synopsis_label
-        self.size_on_disk_label = self.card.size_on_disk_label
-
-        self.add_button.connect('clicked', self.card.on_add_button_clicked)
-        self.resume_button.connect('clicked', self.card.on_resume_button_clicked)
-
-        self.window.breakpoint.add_setter(self.title_box, 'orientation', Gtk.Orientation.VERTICAL)
-        self.window.breakpoint.add_setter(self.title_box, 'spacing', 12)
-        self.window.breakpoint.add_setter(self.name_label, 'halign', Gtk.Align.CENTER)
-        self.window.breakpoint.add_setter(self.name_label, 'justify', Gtk.Justification.CENTER)
-        self.window.breakpoint.add_setter(self.status_server_label, 'halign', Gtk.Align.CENTER)
-        self.window.breakpoint.add_setter(self.status_server_label, 'justify', Gtk.Justification.CENTER)
-        self.window.breakpoint.add_setter(self.authors_label, 'halign', Gtk.Align.CENTER)
-        self.window.breakpoint.add_setter(self.authors_label, 'justify', Gtk.Justification.CENTER)
-        self.window.breakpoint.add_setter(self.buttons_box, 'orientation', Gtk.Orientation.VERTICAL)
-        self.window.breakpoint.add_setter(self.buttons_box, 'spacing', 18)
-        self.window.breakpoint.add_setter(self.buttons_box, 'halign', Gtk.Align.CENTER)
-
-    def populate(self):
-        manga = self.card.manga
-
-        # Name
-        self.name_label.set_text(manga.name)
-
-        # Cover
-        if self.cover_picture:
-            self.cover_box.remove(self.cover_picture)
-
-        if manga.cover_fs_path is None:
-            self.cover_picture = CoverPicture.new_from_resource(MISSING_IMG_RESOURCE_PATH, width=COVER_WIDTH)
-            self.card.remove_backdrop()
-        else:
-            picture = CoverPicture.new_from_file(manga.cover_fs_path, width=COVER_WIDTH)
-            if picture:
-                self.cover_picture = picture
-                self.card.set_backdrop()
-            else:
-                self.cover_picture = CoverPicture.new_from_resource(MISSING_IMG_RESOURCE_PATH, width=COVER_WIDTH)
-                self.card.remove_backdrop()
-
-        self.cover_picture.props.can_shrink = False
-        self.cover_picture.add_css_class('cover-dropshadow')
-        self.cover_box.append(self.cover_picture)
-
-        # Authors
-        authors = html_escape(', '.join(manga.authors)) if manga.authors else _('Unknown author')
-        self.authors_label.set_markup(authors)
-
-        # Server (link to server page)
-        if not manga.is_local:
-            self.status_server_label.set_markup(
-                '{0} · <a href="{1}">{2}</a> ({3})'.format(
-                    _(manga.STATUSES[manga.status]) if manga.status else _('Unknown status'),
-                    manga.server.get_manga_url(manga.slug, manga.url),
-                    html_escape(manga.server.name),
-                    manga.server.lang.upper() if manga.server.lang else '??'
-                )
-            )
-        else:
-            self.status_server_label.set_text('{0} · {1}'.format(_('Unknown status'), _('Local')))
-
-        # Resume button
-        if manga.in_library:
-            self.add_button.set_visible(False)
-            self.resume_button.add_css_class('suggested-action')
-        else:
-            self.add_button.set_visible(True)
-            self.resume_button.remove_css_class('suggested-action')
-
-        # Genres
-        if manga.genres:
-            self.genres_wrapbox.remove_all()
-
-            for genre in sorted(manga.genres):
-                label = Gtk.Label()
-                label.set_ellipsize(Pango.EllipsizeMode.END)
-                label.set_markup(html_escape(genre))
-                label.set_css_classes(['genre-label', 'caption'])
-                self.genres_wrapbox.append(label)
-
-            self.genres_wrapbox.get_parent().set_visible(True)
-        else:
-            self.genres_wrapbox.get_parent().set_visible(False)
-
-        # Categories
-        self.set_categories()
-
-        # Scanlators
-        if manga.scanlators:
-            self.scanlators_label.set_markup(html_escape(', '.join(manga.scanlators)))
-            self.scanlators_label.get_parent().set_visible(True)
-        else:
-            self.scanlators_label.get_parent().set_visible(False)
-
-        # Number of chapters
-        self.chapters_label.set_text(str(len(manga.chapters)))
-
-        # Last update date
-        if manga.last_update:
-            self.last_update_label.set_text(
-                manga.last_update.replace(tzinfo=pytz.UTC).astimezone(TIMEZONE).strftime(_('%m/%d/%Y %H:%M'))
-            )
-            self.last_update_label.get_parent().set_visible(True)
-        else:
-            self.last_update_label.get_parent().set_visible(False)
-
-        # Disk usage
-        self.set_disk_usage()
-
-        # Synopsis
-        self.synopsis_label.set_text('-')
-        if manga.synopsis:
-            synopsis = manga.synopsis
-            if manga.server.donate_url:
-                synopsis = f'{synopsis}\n\n<a href="{manga.server.donate_url}">{_("Donate")}</a>'
-
-            self.synopsis_label.set_markup(synopsis)  # can failed with a warning: parsing markup error
-
-    def refresh(self):
-        self.set_disk_usage()
-
-    def set_categories(self):
-        if self.card.manga.categories:
-            self.categories_wrapbox.remove_all()
-
-            for category_id in sorted(self.card.manga.categories):
-                category = Category.get(category_id)
-                label = Gtk.Label()
-                label.set_ellipsize(Pango.EllipsizeMode.END)
-                label.set_markup(html_escape(category.label))
-                label.set_css_classes(['category-label', 'caption'])
-                self.categories_wrapbox.append(label)
-
-            self.categories_wrapbox.get_parent().set_visible(True)
-        else:
-            self.categories_wrapbox.get_parent().set_visible(False)
-
-    def set_disk_usage(self):
-        self.size_on_disk_label.set_text(folder_size(self.card.manga.path) or '-')
---
+++ komikku/card/synopsis_fading.py
@@ -0,0 +1,197 @@
+# SPDX-FileCopyrightText: 2019-2026 Valéry Febvre
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Author: Valéry Febvre <vfebvre@easter-eggs.com>
+
+from gi.repository import Adw
+from gi.repository import Gdk
+from gi.repository import GLib
+from gi.repository import GObject
+from gi.repository import Graphene
+from gi.repository import Gsk
+from gi.repository import Gtk
+
+
+class SynopsisFading(Gtk.Widget, Gtk.ConstraintTarget):
+    __gtype_name__ = 'SynopsisFading'
+
+    def __init__(self):
+        super().__init__()
+
+        self.__max_height = 95
+        self.__faded = False
+
+        self.min_height = self.__max_height
+        self.current_height = 0
+        self.allocated_width = 0
+
+        self.animation = Adw.SpringAnimation(
+            widget=self,
+            value_from=0,
+            value_to=1,
+            spring_params=Adw.SpringParams.new(damping_ratio=0.5, mass=1, stiffness=50),
+            target=Adw.CallbackAnimationTarget.new(self.animate),
+            initial_velocity=1,
+            epsilon=0.001,
+            clamp=True
+        )
+        self.child = None
+
+    @GObject.Property(type=bool, default=False)
+    def faded(self):
+        return self.__faded
+
+    @faded.setter
+    def faded(self, value):
+        self.__faded = value
+
+    @GObject.Property(type=int, default=95)
+    def max_height(self):
+        return self.__max_height
+
+    @max_height.setter
+    def max_height(self, value):
+        if self.max_height == value:
+            return
+
+        self.__max_height = value
+
+        if self.child:
+            if self.allocated_width > 0:
+                width = self.allocated_width
+            else:
+                _, width, _, _ = self.child.measure(Gtk.Orientation.HORIZONTAL, -1)
+
+            _, child_height, _, _ = self.child.measure(Gtk.Orientation.VERTICAL, width)
+            if child_height <= value:
+                target_height = child_height
+            else:
+                target_height = value
+        else:
+            target_height = value
+
+        self.animation_height_start = self.current_height
+        self.animation_height_end = target_height
+        self.animation.play()
+
+    def animate(self, value):
+        self.current_height = Adw.lerp(self.animation_height_start, self.animation_height_end, value)
+        self.queue_resize()
+
+    def do_measure(self, orientation, for_size):
+        if not self.child:
+            return 0, 0, -1, -1
+
+        if orientation == Gtk.Orientation.HORIZONTAL:
+            return self.child.measure(orientation, for_size)
+
+        if for_size == -1:
+            if self.allocated_width > 0:
+                for_size = self.allocated_width
+            else:
+                _, for_size, _, _ = self.child.measure(Gtk.Orientation.HORIZONTAL, -1)
+
+        _, child_height, _, _ = self.child.measure(Gtk.Orientation.VERTICAL, for_size)
+        if child_height <= self.max_height:
+            target_height = child_height
+        else:
+            target_height = self.max_height
+
+        if self.animation.get_state() == Adw.AnimationState.IDLE:
+            self.current_height = target_height
+
+        return self.current_height, self.current_height, -1, -1
+
+    def do_size_allocate(self, width, height, baseline):
+        if self.allocated_width != width:
+            self.allocated_width = width
+            if self.child:
+                _, child_height, _, _ = self.child.measure(Gtk.Orientation.VERTICAL, width)
+
+                if self.min_height != self.max_height or child_height < self.min_height:
+                    self.current_height = child_height
+                GLib.idle_add(self.queue_resize)
+
+        if self.child:
+            _, child_height, _, _ = self.child.measure(Gtk.Orientation.VERTICAL, width)
+            if child_height > height:
+                child_height = child_height
+            else:
+                child_height = height
+
+            self.child.allocate(width, child_height, baseline, None)
+
+        self.update_faded()
+
+    def do_snapshot(self, snapshot):
+        if not self.child:
+            return
+
+        width = self.get_width()
+        height = self.get_height()
+        if height <= 0:
+            return
+
+        _, child_height, _, _ = self.child.measure(Gtk.Orientation.VERTICAL, width)
+        if child_height <= height:
+            self.snapshot_child(self.child, snapshot)
+            return
+
+        # Fading
+        snapshot.push_mask(Gsk.MaskMode.ALPHA)
+
+        stop_offset = Adw.lerp(0.25, 1, (height - self.min_height) / (child_height - self.min_height))
+
+        cs0 = Gsk.ColorStop()
+        cs0.color = Gdk.RGBA(1, 1, 1, 1)
+        cs0.offset = stop_offset
+        cs1 = Gsk.ColorStop()
+        cs1.color = Gdk.RGBA(1, 1, 1, 0)
+        cs1.offset = 1
+
+        snapshot.append_linear_gradient(
+            Graphene.Rect.init(Graphene.Rect.alloc(), 0, 0, width, height),
+            Graphene.Point.init(Graphene.Point.alloc(), 0, 0),
+            Graphene.Point.init(Graphene.Point.alloc(), 0, height),
+            [cs0, cs1]
+        )
+
+        snapshot.pop()
+
+        self.snapshot_child(self.child, snapshot)
+
+        snapshot.pop()
+
+    def get_request_mode(self):
+        return Gtk.SizeRequestMode.HEIGHT_FOR_WIDTH
+
+    def set_child(self, child):
+        if self.child:
+            self.child.unparent()
+
+        self.child = child
+        self.child.set_parent(self)
+
+    def set_markup(self, text):
+        self.__faded = None
+        self.__max_height = self.min_height
+        self.current_height = self.min_height
+        self.allocated_width = 0
+        self.child.set_markup(text)
+
+    def set_revealed(self, revealed):
+        self.max_height = self.child.get_height() if revealed else self.min_height
+
+    def update_faded(self):
+        new_value = False
+
+        if self.child:
+            if self.allocated_width > 0:
+                width = self.allocated_width
+            else:
+                _, width, _, _ = self.child.measure(Gtk.Orientation.HORIZONTAL, -1)
+            _, child_height, _, _ = self.child.measure(Gtk.Orientation.VERTICAL, width)
+
+            new_value = child_height > self.min_height
+
+        if self.faded != new_value:
+            self.faded = new_value

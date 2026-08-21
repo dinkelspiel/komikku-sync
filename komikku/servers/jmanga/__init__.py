9682ed84846ee975ccefa894f9a590fa69ce72c8
Valéry Febvre <vfebvre@easter-eggs.com>
2026-08-21 22:45:18 +0200

    [Servers] JManga (JA): Update
    Domain change

--- komikku/servers/jmanga/__init__.py
+++ komikku/servers/jmanga/__init__.py
@@ -13,7 +13,7 @@ class Jmanga(Mangareader):
     lang = 'ja'
     is_nsfw = True
 
-    base_url = 'https://jmanga.farm'
+    base_url = 'https://jmanga.land'
     list_url = base_url + '/filter/'
     search_url = base_url + '/'
     manga_url = base_url + '/read/{0}/'
